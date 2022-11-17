bd6b20db979151b0b303a8074ebc89160d6267f5
panagop <gpanagop@gmail.com>
2022-11-17 11:39:51 +0200

    -

--- LamPaStreamlit.py
+++ LamPaStreamlit.py
@@ -22,9 +22,9 @@ st.title('1-D Seismic Site Response Analysis')
 st.text('This is a web app for one dimensional ground response analysis \nusing data from seismic motions for the development of \nAcceleration Response Spectra and Spectral Ratio \nPystrata library etc etc')
 
 # load ini json file
-# lpsi = LPyStrataInput.from_json_file('ini.json')
-with open('ini.pickle', "rb") as pfile:
-    lpsi = pickle.load(pfile)
+lpsi = LPyStrataInput.from_json_file('streamlit\ini.json')
+# with open('ini.pickle', "rb") as pfile:
+#     lpsi = pickle.load(pfile)
 
 lproject = LProject(lpsi)
 
